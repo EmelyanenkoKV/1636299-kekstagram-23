34b53fb61ca4a5c158bf3415588019db0cc1f63f
EmelyanenkoKV <emelyanenkoKV@gmail.com>
2021-05-31 14:03:36 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-const GETTING_VALUE = function (minValue, maxValue) {
+const getValue = function (minValue, maxValue) {
   if (minValue < 0 || maxValue < 0) {
-    return ('Значение меньше чем ноль');
+    return 'Значение меньше чем ноль';
   } else if (minValue > maxValue) {
-    return ('Поменяйте числа местами');
+    return 'Поменяйте числа местами';
   } else if (minValue === maxValue) {
-    return ('Введенные числа равны');
+    return 'Введенные числа равны';
   }
   return Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
 };
 
-GETTING_VALUE();
+getValue();
 
-const CHECK_LENGHT = function (string, maxStringLenght) {
+const checkLenght = function (string, maxStringLenght) {
   return string.length <= maxStringLenght;
 };
 
-CHECK_LENGHT();
+checkLenght();
