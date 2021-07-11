0f29f0daced4f2d1dd674e5d7d5feeaee8a41858
EmelyanenkoKV <emelyanenkoKV@gmail.com>
2021-07-12 02:49:29 +0300

    Работа с сервером

---
+++ js/api.js
@@ -0,0 +1,36 @@
+import {renderSimilarPhoto} from './preview.js';
+import {addBigPhotoComments} from './full-photo.js';
+import {onFormSuccessSend} from './modal-messages.js';
+import {onFormErrorSend} from './modal-messages.js';
+import {showAlert} from './utils.js';
+
+const imgUploadForm = document.querySelector('.img-upload__form');
+
+const getUserPhotos = () => {
+  fetch('https://23.javascript.pages.academy/kekstagram/data')
+    .then((response) => response.json())
+    .then((userPhotos) => {
+      renderSimilarPhoto(userPhotos);
+      addBigPhotoComments(userPhotos);
+    })
+    .catch(() => showAlert ('Ошибка при загрузке фото. Попробуйте ещё раз'));
+};
+getUserPhotos();
+
+
+const setUserFormSubmit = (onSuccess, onError) => {
+  imgUploadForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    fetch(
+      'https://23.javascript.pages.academy/kekstagram',
+      {
+        method: 'POST',
+        body: new FormData(evt.target),
+      })
+      .then(() => onSuccess())
+      .catch(() => onError());
+  });
+};
+
+setUserFormSubmit(onFormSuccessSend, onFormErrorSend);
